fb30d96a1370326f6cc8120b373a4cb3f3b6329f
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-12-24 11:04:22 -0600

    added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "catalogue" {
     ami = "ami-0b4f379183e5706b9"
     instance_type = "t3.medium"
-    vpc_security_groups = ["sg-01e1a404b2d4535f4"]
+    vpc_security_group_ids = ["sg-01e1a404b2d4535f4"]
     //user_data = file("catalogue.sh")
     tags = {
         Name = "catalogue-dev-ami"
